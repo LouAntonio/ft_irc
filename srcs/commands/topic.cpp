ab76fd213618a380c17edbe4c4e14614f31edab8
LouAntonio <lourencodala88@gmail.com>
2026-02-03 10:27:59 +0100

    Channel::setTopic method

--- srcs/commands/topic.cpp
+++ srcs/commands/topic.cpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 09:35:28 by lantonio          #+#    #+#             */
-/*   Updated: 2026/02/03 09:50:54 by lantonio         ###   ########.fr       */
+/*   Updated: 2026/02/03 10:14:36 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@ std::string	Server::_topic(commandRequest& request, int sender_fd) {
 	if (!_clients[sender_fd]->isAuth())
 		return ":localhost 451 * :You have not registered\r\n";
 
-	std::string	channel = nullptr;
+	std::string	channel;
 	if (request.args.size())
 	{
 		if (request.args[0][0] != '#')
 			return ":localhost 461 * :Not enounth params\r\n";
-		
+		if (request.args.size() == 1)
+		{
+			// channel exists and user is in it
+			if (_channels.find(request.args[0]) != _channels.end() && _channels[request.args[0]]->isMember(sender_fd))
+				return ":localhost 332 * :No topic setted to channel\r\n";
+		}
 	}
 	return ":localhost 461 * :Not enounth params\r\n";
 }
